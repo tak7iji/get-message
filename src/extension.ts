351b1d3ee49c2e7a94d0e8edeb629257de22a177
E.T <tak7iji-at-gmail-dot-com>
2022-06-17 20:58:35 +0900

    Promiseを返すように修正

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import fs = require('fs');
-import readline = require('readline');
 import path = require('path');
 import * as vscode from 'vscode';
 
@@ -13,39 +12,40 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "view-tc-log-message" is now active!');
 
-	const disposable = vscode.languages.registerHoverProvider('java', {
+	var disposable = vscode.languages.registerHoverProvider('java', {
 		provideHover(document, position, token) {
 			const range = document.getWordRangeAtPosition(position, /\"([a-zA-Z]+\.)+[a-zA-Z]+\"/);
-			const word = document.getText(range);
 			
 			if (typeof range !== 'undefined') {
-				var contents:Buffer;
-				var lines:string[] = [];
-				var propertyFile = path.dirname(document.fileName)+path.sep+'LocalStrings_ja.properties';
-				try {
-					contents = fs.readFileSync(propertyFile);
-					lines = contents.toString().split('\n');
-				} catch(err) {}
-
-
-				propertyFile = path.dirname(document.fileName)+path.sep+'LocalStrings.properties';
-				try {
-					contents = fs.readFileSync(propertyFile);
-					lines.concat(contents.toString().split('\n'));
-				} catch(err) {}
-
-				const regExp = new RegExp(`${word.slice(1,-1)}=(.*)`);
-				var message:string = "";
-
-				for (var i in lines) {
-					const result = lines[i].match(regExp);
-					if (result !== null) {
-						message = result.at(1)!;
-						break;
-					}
-				};
-				return new vscode.Hover(message);
+				return new Promise(resolve => {
+					const word = document.getText(range);
+					var contents:Buffer;
+					var lines:string[] = [];
+					var propertyFile = path.dirname(document.fileName)+path.sep+'LocalStrings_ja.properties';
+					try {
+						contents = fs.readFileSync(propertyFile);
+						lines = contents.toString().split('\n');
+					} catch(err) {}
+	
+	
+					propertyFile = path.dirname(document.fileName)+path.sep+'LocalStrings.properties';
+					try {
+						contents = fs.readFileSync(propertyFile);
+						lines.concat(contents.toString().split('\n'));
+					} catch(err) {}
+	
+					const regExp = new RegExp(`${word.slice(1,-1)}=(.*)`);
+					var message:string = "";
+	
+					for (var i in lines) {
+						const result = lines[i].match(regExp);
+						if (result !== null) {
+							resolve(new vscode.Hover(result.at(1)!));
+						}
+					};	
+				});
 			}
+			return;
 		}
 	});
 
