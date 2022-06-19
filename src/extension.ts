b185ad3ecf6ca5d732b363f820f2c63a00c5f7c7
E.T <tak7iji-at-gmail-dot-com>
2022-06-19 13:32:35 +0900

    リファクタリング

--- src/extension.ts
+++ src/extension.ts
@@ -1,13 +1,13 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import fs = require('fs');
-import path = require('path');
+// Import the module and {reference} it with the alias vscode in your code below
+import * as fs from 'fs';
+import * as path from 'path';
 import * as vscode from 'vscode';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	
+
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "view-tc-log-message" is now active!');
@@ -15,43 +15,28 @@ export function activate(context: vscode.ExtensionContext) {
 	var disposable = vscode.languages.registerHoverProvider('java', {
 		provideHover(document, position, token) {
 			const range = document.getWordRangeAtPosition(position, /\"([a-zA-Z]+\.)+[a-zA-Z]+\"/);
-			const config = vscode.workspace.getConfiguration('conf.viewTcLogMessage');
-			const locale = config.get<string>('locale');
+			const locale = vscode.workspace.getConfiguration().get('conf.viewTcLogMessage.locale');
 
 			if (typeof range !== 'undefined') {
 				return new Promise((resolve) => {
-					const key:string = document.getText(range).slice(1, -1);
-					var contents:Buffer;
-					let lines:string[] = [];
-					var message:string[] = [];
+					const baseName = path.dirname(document.fileName) + path.sep + 'LocalStrings';
+					const extName = '.properties';
+					const key: string = document.getText(range).slice(1, -1);
+					let message: string[] = [];
 
-					propertyFile = path.dirname(document.fileName)+path.sep+'LocalStrings.properties';
-					try {
-						contents = fs.readFileSync(propertyFile);
-						contents.toString().split('\n').forEach((line) => {
-							if (line.startsWith(key)) {
-								message[0] = (line.slice(key.length+1));
-							}
-						});
-					} catch(err) {}
-
-					if (locale !== '') {
-						var propertyFile = path.dirname(document.fileName)+path.sep+'LocalStrings_'+locale+'.properties';
+					[baseName+extName, baseName+'_'+locale+extName].map((propertyFile,idx) => {
 						try {
-							contents = fs.readFileSync(propertyFile);
-							contents.toString().split('\n').forEach((line) => {
+							fs.readFileSync(propertyFile).toString().split('\n').forEach((line) => {
 								if (line.startsWith(key)) {
-									message[1] = line.slice(key.length+1);
+									message[idx] = line.slice(key.length + 1);
 								}
 							});
-						} catch(err) {
-						}	
-					}
-	
+						} catch (err) {	}	
+					});
+
 					resolve(new vscode.Hover(message));
 				});
 			}
-			return;
 		}
 	});
 
@@ -59,4 +44,4 @@ export function activate(context: vscode.ExtensionContext) {
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
